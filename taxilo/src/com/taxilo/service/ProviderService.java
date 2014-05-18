4a19f4b13c3823bed8bc0275225470f88a6fcc3b
vaibhav4u06 <vaibhav4u06@gmail.com>
2014-05-19 03:20:52 +0530

    documentation

--- taxilo/src/com/taxilo/service/ProviderService.java
+++ taxilo/src/com/taxilo/service/ProviderService.java
@@ -9,12 +9,59 @@ import com.taxilo.pojo.Provider;
 import com.taxilo.pojo.Rating;
 
 public interface ProviderService {
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws UnknownHostException
+	 */
 	public Provider getProviderById(String id)throws UnknownHostException;
+	/**
+	 * 
+	 * @param providerId
+	 * @return
+	 * @throws UnknownHostException
+	 */
 	public Provider getProviderByProviderId(Integer providerId)throws UnknownHostException;
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws UnknownHostException
+	 */
 	public List<Rating> getRatingsForProvider(String id)throws UnknownHostException;
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws UnknownHostException
+	 */
 	public List<Edge> getEdgesForProvider(String id)throws UnknownHostException;
+	/**
+	 * 
+	 * @param id
+	 * @return
+	 * @throws UnknownHostException
+	 */
 	public List<String> getApplicableCitiesForProvider(String id)throws UnknownHostException;
+	/**
+	 * 
+	 * @param provider
+	 * @throws UnknownHostException
+	 */
 	public void saveProvider(Provider provider)throws UnknownHostException;
+	/**
+	 * 
+	 * @param id
+	 * @param cars
+	 * @throws UnknownHostException
+	 */
 	public void addCarsForProvider(String id,List<Car> cars)throws UnknownHostException;
+	/**
+	 * 
+	 * @param id
+	 * @param rating
+	 * @throws UnknownHostException
+	 */
 	public void addRatingForProvider(String id,Rating rating)throws UnknownHostException;
 }
